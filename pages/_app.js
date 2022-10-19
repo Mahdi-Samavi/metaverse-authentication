36ba6e38971bced8bea3e188784ac7fd98a3b66d
Mahdi Samavi <samavi.mahdi@gmail.com>
2022-10-19 15:43:31 +0330

    login flow with Moralis completed

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import Head from 'next/head'
+import { MoralisProvider } from 'react-moralis'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider appId={process.env.NEXT_PUBLIC_APP_ID} serverUrl={process.env.NEXT_PUBLIC_SERVER_URL}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </MoralisProvider>
+  )
 }
 
 export default MyApp
